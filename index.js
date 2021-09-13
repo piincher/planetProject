700130dc697b0796f2483c8b08d176f161ac4ceb
Ibrahim Kouma <ikouma269@gmail.com>
2021-09-13 20:37:23 +0800

    finding the habitable planet

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const parse = require('csv-parse');
 const fs = require('fs');
-const results = [];
+const habitablePlanets = [];
+
+function isHabitable(planet) {
+	return (
+		planet['koi_disposition'] === 'CONFIRMED' &&
+		planet['koi_insol'] > 0.36 &&
+		planet['koi_insol'] < 1.11 &&
+		planet['koi_prad'] < 1.6
+	);
+}
 
 fs
 	.createReadStream('kepler_data.csv')
@@ -11,12 +20,13 @@ fs
 		})
 	)
 	.on('data', (data) => {
-		results.push(data);
+		if (isHabitable(data)) {
+			habitablePlanets.push(data);
+		}
 	})
 	.on('error', (error) => {
 		console.log(error);
 	})
 	.on('end', () => {
-		console.log(results);
-		console.log('done');
+		console.log(` the habitable planets are ${habitablePlanets.length}`);
 	});
