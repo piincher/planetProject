9bace3903fcf8b542a197523a82c1fb907e33d41
Ibrahim Kouma <ikouma269@gmail.com>
2021-09-13 20:21:00 +0800

    parsing our planet data

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const results = [];
 
 fs
 	.createReadStream('kepler_data.csv')
+	.pipe(
+		parse({
+			comment: '#',
+			columns: true
+		})
+	)
 	.on('data', (data) => {
 		results.push(data);
 	})
