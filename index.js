e23d6cd67ea2f1fcd8ca2048ca2313fda269960b
Ibrahim Kouma <ikouma269@gmail.com>
2021-09-13 20:03:59 +0800

    read file

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const parse = require('csv-parse');
+const fs = require('fs');
+const results = [];
+
+fs
+	.createReadStream('kepler_data.csv')
+	.on('data', (data) => {
+		results.push(data);
+	})
+	.on('error', (error) => {
+		console.log(error);
+	})
+	.on('end', () => {
+		console.log(results);
+		console.log('done');
+	});
